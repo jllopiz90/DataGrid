7489aace78923eec696d65cce7d9cf641da40de7
jllopiz <jllopiz@1nationgfx.com>
2021-04-28 12:14:34 -0400

    use DataGrid with test data as example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import DataGrid from "./components/DataGrid";
+
+const testColumns = [
+  { name: "name", title: "Name" },
+  { name: "phone", title: "Phone" },
+  { name: "isActive", title: "Active" },
+  { name: "address", title: "Adress" },
+];
+
+const renderTestCells = (columnName: string, value: any, key: string) => {
+  if (columnName === "isActive") {
+    return (
+      <td key={key} className="px-6 py-4 whitespace-nowrap">
+        {value ? (
+          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+            Yes
+          </span>
+        ) : (
+          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-green-800">
+            No
+          </span>
+        )}
+      </td>
+    );
+  }
+  return (
+    <td key={key} className="px-6 py-4 whitespace-nowrap">
+      {value}
+    </td>
+  );
+};
+
+const testData = [
+  {
+    name: "Tester Testerson",
+    phone: "(111) 111-1111",
+    isActive: false,
+    address: "123 st test Av Miami Fl 33133",
+  },
+  {
+    name: "Testerin Teststar",
+    phone: "(222) 222-2222",
+    isActive: true,
+    address: "456 st test Av Miami Fl 33133",
+  },
+  {
+    name: "Testerina IV",
+    phone: "(333) 333-3333",
+    isActive: false,
+    address: "789 st test Av Miami Fl 33133",
+  },
+];
 
 function App() {
   return (
-    <div className="flex justify-center py-10 bg-gray-400">
-      <span className="text-green-400 text-xl font-bold">Welcome to Tailwindcss</span>
+    <div className="flex flex-col justify-center items-center py-10 bg-gray-400">
+      <span className="text-green-400 text-xl font-bold">Datagrid example</span>
+      <div className="mt-10">
+        <DataGrid
+          columns={testColumns}
+          data={testData}
+          renderCell={renderTestCells}
+        />
+      </div>
     </div>
   );
 }
